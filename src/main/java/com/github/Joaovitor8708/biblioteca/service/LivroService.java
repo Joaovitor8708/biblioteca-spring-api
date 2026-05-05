473cdfada3967f162bc93c699736986109288bf6
Joaovitor8708 <vitorsantanalins@gmail.com>
2026-05-05 08:37:50 -0300

    feat: implementa LivroService com listarTodos, buscarPorId, buscarPorCategoria e adicionar

--- src/main/java/com/github/Joaovitor8708/biblioteca/service/LivroService.java
+++ src/main/java/com/github/Joaovitor8708/biblioteca/service/LivroService.java
@@ -1,7 +1,39 @@
 package com.github.Joaovitor8708.biblioteca.service;
 
+import com.github.Joaovitor8708.biblioteca.model.Livro;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LivroService {
+    private final List<Livro> livro;
+
+    public LivroService(@Qualifier("repositorioOrdenado") List<Livro> livro) {
+        this.livro = livro;
+    }
+
+    public List<Livro> listarTodos(){
+        return livro;
+    }
+
+    public Livro buscarPorId(Long id){
+        return livro.stream()
+                .filter(livro -> livro.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Livro> buscarPorCategoria(String categoria){
+        return livro.stream()
+                .filter(livro -> livro.getCategoria().equals(categoria))
+                .collect(Collectors.toList());
+    }
+
+    public Livro adicionarLivro(Livro novoLivro){
+       livro.add(novoLivro);
+       return novoLivro;
+    }
 }
