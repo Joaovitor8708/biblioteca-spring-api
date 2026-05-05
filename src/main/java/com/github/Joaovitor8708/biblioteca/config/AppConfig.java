c61fa8d71b696fb575c0d25fbcbca5ee7bb62376
Joaovitor8708 <vitorsantanalins@gmail.com>
2026-05-04 22:56:16 -0300

    feat: cria AppConfig com beans repositorioSimples e repositorioOrdenado

--- src/main/java/com/github/Joaovitor8708/biblioteca/config/AppConfig.java
+++ src/main/java/com/github/Joaovitor8708/biblioteca/config/AppConfig.java
@@ -1,8 +1,28 @@
 package com.github.Joaovitor8708.biblioteca.config;
 
+import com.github.Joaovitor8708.biblioteca.model.Livro;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    public List<Livro> repositorioSimples() {
+        List<Livro> livros = new ArrayList<>();
+        livros.add(new Livro(1L, "O Hobbit", "Tolkien", "Fantasia"));
+        livros.add(new Livro(2L, "Clean Code", "Robert Martin", "Tecnologia"));
+        livros.add(new Livro(3L, "Dom Casmurro", "Machado de Assis", "Literatura"));
+        return livros;
+    }
 
+    @Bean
+    public List<Livro> repositorioOrdenado() {
+        List<Livro> livros = repositorioSimples();
+        livros.sort(Comparator.comparing(Livro::getTitulo));
+        return livros;
+    }
 }
